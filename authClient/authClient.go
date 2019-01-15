87002e2f07ad7d700d3a381eb95e5c77457c62d5
Levi Sky Schoen <levi@tozny.com>
2019-01-15 12:31:05 -0800

    Add client code and integration test for calling auth service validate endpoint. Standardize capitalization of client naming.

--- authClient/authClient.go
+++ authClient/authClient.go
@@ -3,7 +3,9 @@
 package authClient
 
 import (
+    "bytes"
     "context"
+    "encoding/json"
     "fmt"
     "github.com/tozny/e3db-clients-go"
     "golang.org/x/oauth2"
@@ -15,8 +17,8 @@ const (
     AuthServiceBasePath = "v1/auth" //HTTP PATH prefix for calls to the auth service.
 )
 
-//E3DBAuthClient implements an http client for communication with an e3db auth service.
-type E3DBAuthClient struct {
+//E3dbAuthClient implements an http client for communication with an e3db auth service.
+type E3dbAuthClient struct {
     APIKey       string
     APISecret    string
     Host         string
@@ -25,19 +27,34 @@ type E3DBAuthClient struct {
 
 // GetToken attempts to retrieve and return a valid oauth2 token based on the clients
 // credentials, returning token and error(if any).
-func (c *E3DBAuthClient) GetToken(ctx context.Context) (*oauth2.Token, error) {
+func (c *E3dbAuthClient) GetToken(ctx context.Context) (*oauth2.Token, error) {
     return c.oauth2Helper.Token(ctx)
+}
+
+func (c *E3dbAuthClient) ValidateToken(ctx context.Context, params ValidateTokenRequest) (*ValidateTokenResponse, error) {
+    var result *ValidateTokenResponse
+    var buf bytes.Buffer
+    err := json.NewEncoder(&buf).Encode(&params)
+    if err != nil {
+        return result, err
+    }
+    request, err := http.NewRequest("POST", c.Host+"/"+AuthServiceBasePath+"/validate", &buf)
+    if err != nil {
+        return result, err
+    }
+    err = e3dbClients.MakeE3DBServiceCall(c, ctx, request, &result)
+    return result, err
 }
 
 // AuthHTTPClient returns an http client that can be used for
 // making authenticated requests to an e3db endpoint using the provided context.
-func (c *E3DBAuthClient) AuthHTTPClient(ctx context.Context) *http.Client {
+func (c *E3dbAuthClient) AuthHTTPClient(ctx context.Context) *http.Client {
     return c.oauth2Helper.Client(ctx)
 }
 
-// New returns a new E3DBAuthClient configured with the specified apiKey and apiSecret values.
-func New(config e3dbClients.ClientConfig) E3DBAuthClient {
-    return E3DBAuthClient{
+// New returns a new E3dbAuthClient configured with the specified apiKey and apiSecret values.
+func New(config e3dbClients.ClientConfig) E3dbAuthClient {
+    return E3dbAuthClient{
         APIKey:    config.APIKey,
         APISecret: config.APISecret,
         Host:      config.Host,
