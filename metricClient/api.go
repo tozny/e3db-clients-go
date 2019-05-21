bbb4f30ab6c7b5b9c0172aea183e1d2645c20451
Michael Lee <willmichael@tozny.com>
2019-05-20 23:54:26 -0700

    MetricClient to call the new endpoints for the Metrics-Service (#28)
    * Add metricClient
    
    * utils-go version 0.0.24 update

---
+++ metricClient/api.go
@@ -0,0 +1,73 @@
+package metricClient
+
+import (
+	"time"
+)
+
+// APIAggregateRequest is used to make an Aggregate call.
+type APIAggregateRequest struct {
+	Range        QueryRange `json:"range"`         // Range is for the time the request was made to Tozny services
+	IncludeEmpty bool       `json:"include_empty"` // Include dates with count of 0, defaults to false.
+	AccountID    string     `json:"account_id"`
+	ClientIDs    []string   `json:"client_ids"`
+}
+
+// APIAggregateResponse is returned from an Aggregate call.
+type APIAggregateResponse struct {
+	AccountID string         `json:"account_id"`
+	Results   []APIAggregate `json:"results"`
+}
+
+// APIAggregate is a single aggregate returned from the Aggregate endpoint.
+type APIAggregate struct {
+	Date  time.Time `json:"date"`  // Date which aggregate corresponds to
+	Count int64     `json:"count"` // Count of API request for the aggregation
+}
+
+// QueryRange contains the time range in which to query.
+type QueryRange struct {
+	Start time.Time `json:"start_time"`
+	End   time.Time `json:"end_time"`
+}
+
+// APIMetricsRequest is the request object used for searching api metrics.
+type APIMetricsRequest struct {
+	NextToken   int         `json:"next_token"`   // Provided when paginating through query results
+	Limit       int         `json:"limit"`        // The max number of results to return.
+	ExcludeLogs bool        `json:"include_logs"` // Whether to return just counts of requests or the full request
+	AccountID   string      `json:"account_id"`   // AccountID that owns the relevant APIMetrics
+	Match       QueryParams `json:"match"`        // Params to match on
+	Exclude     QueryParams `json:"exclude"`      // Params to exclude on
+	Range       QueryRange  `json:"range"`        // Range is for the time the request was made to Tozny services
+}
+
+// APIMetricsResponse is the response object for requests to the metrics search public api
+type APIMetricsResponse struct {
+	NextToken    int         `json:"next_token"`    // Value to use when paginating through query results
+	TotalResults int64       `json:"total_results"` // Total number of metrics matching request, maximum ~10,000 if larger than 10,000 use smaller range
+	Results      []APIMetric `json:"results"`       // Only returned if IncludeLogs is true for the corresponding request
+}
+
+// APIMetric is returned as a list by queries to the metrics search api
+type APIMetric struct {
+	AccountID  string    `json:"account_id"`
+	ClientID   string    `json:"client_id"`
+	Path       string    `json:"request_path"`
+	Body       string    `json:"request_body"`
+	Method     string    `json:"request_method"`
+	StatusCode int       `json:"status_code"`
+	Time       time.Time `json:"request_time"`
+}
+
+// ApiEndpoint includes the path and method of a service call
+type ApiEndpoint struct {
+	Path   string `json:"path"`   // Path of the request, starting after the host
+	Method string `json:"method"` // Method used i.e. GET | POST ...
+}
+
+// QueryParams that are the availalble fields to search for metrics on metrics can be searched on using the public api
+type QueryParams struct {
+	ClientIDs    []string      `json:"client_ids"`
+	APIEndpoints []ApiEndpoint `json:"api_endpoints"`
+	StatusCodes  []int         `json:"status_codes"`
+}
---
+++ metricClient/metricClient.go
@@ -0,0 +1,54 @@
+package metricClient
+
+import (
+	"context"
+	"github.com/tozny/e3db-clients-go"
+	"github.com/tozny/e3db-clients-go/authClient"
+)
+
+var (
+	MetricServiceBasePath = "v1/metric/"
+)
+
+//MetricClient implements an http client for communication with the metrics service.
+type MetricClient struct {
+	APIKey    string
+	APISecret string
+	Host      string
+	*authClient.E3dbAuthClient
+}
+
+// Aggregations gets the aggregation from the metrics service.
+func (c *MetricClient) Aggregations(ctx context.Context, params APIAggregateRequest) (*APIAggregateResponse, error) {
+	var result *APIAggregateResponse
+	path := c.Host + "/" + MetricServiceBasePath + "aggregations"
+	request, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeE3DBServiceCall(c.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+// APIMetrics queries elastic search for API Metrics matching params provided
+func (c *MetricClient) APIMetrics(ctx context.Context, params APIMetricsRequest) (*APIMetricsResponse, error) {
+	var result *APIMetricsResponse
+	path := c.Host + "/" + MetricServiceBasePath
+	request, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeE3DBServiceCall(c.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+// New returns a new E3dbSearchIndexerClient for authenticated communication with a Search Indexer service at the specified endpoint.
+func New(config e3dbClients.ClientConfig) MetricClient {
+	authService := authClient.New(config)
+	return MetricClient{
+		config.APIKey,
+		config.APISecret,
+		config.Host,
+		&authService,
+	}
+}
