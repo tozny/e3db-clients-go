2f7e90c848bd9f64265ce010984adda289606229
Eli Fabens <efabens@tozny.com>
2019-06-12 14:41:40 -0700

    More billing service functions

--- billingClient/api.go
+++ billingClient/api.go
@@ -1,6 +1,10 @@
 package billingclient
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type CreateCustomerRequest struct {
 	AccountID       uuid.UUID `json:"account_id"`
@@ -10,5 +14,24 @@ type CreateCustomerRequest struct {
 }
 
 type CreateCustomerResponse struct {
-	StripeID string `json:"stripe+id"`
+	StripeID string `json:"stripe_id"`
+}
+
+// InternalGetMeteredSubscriptionInfoResponse represents a response from calling the billing service
+// internal/v1/billing/subscription-info/{account-id} endpoint
+type InternalGetMeteredSubscriptionInfoResponse struct {
+	StripeID                       string    `json:"stripe_id"`
+	SubscriptionID                 string    `json:"stripe_subscription_id"`
+	SubscriptionItemID             string    `json:"stripe_subscription_item_id"`
+	SubscriptionCreatedAt          time.Time `json:"subscription_created_at"`
+	SubscriptionCurrentPeriodStart time.Time `json:"subscription_current_period_start"`
+	SubscriptionCurrentPeriodEnd   time.Time `json:"subscription_current_period_end"`
+}
+
+type GetAccountStatusResponse struct {
+	AccountActive   bool      `json:"account_active"`
+	IsTrial         bool      `json:"is_trial"`
+	TrialPeriodEnds time.Time `json:"trial_period_ends"`
+	IsLegacy        bool      `json:"is_legacy"`
+	IsGoodStanding  bool      `json:"is_good_standing"`
 }
--- billingClient/billingClient.go
+++ billingClient/billingClient.go
@@ -3,6 +3,7 @@ package billingclient
 import (
 	"context"
 
+	"github.com/google/uuid"
 	e3dbClients "github.com/tozny/e3db-clients-go"
 	"github.com/tozny/e3db-clients-go/authClient"
 )
@@ -21,8 +22,52 @@ type E3dbBillingClient struct {
 
 func (b *E3dbBillingClient) CreateAndSubscribeCustomer(ctx context.Context, params CreateCustomerRequest) (*CreateCustomerResponse, error) {
 	var result *CreateCustomerResponse
-	path := b.Host + "/internal/" + BillingServiceBasePath + "/create_customer"
+	path := b.Host + "/internal/" + BillingServiceBasePath + "/create-customer"
 	request, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, e3dbClients.NewError(err.Error(), path, 0)
+	}
+	err = e3dbClients.MakeE3DBServiceCall(b.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+func (b *E3dbBillingClient) InternalSubscriptionInfo(ctx context.Context, accountID uuid.UUID) (*InternalGetMeteredSubscriptionInfoResponse, error) {
+	var result *InternalGetMeteredSubscriptionInfoResponse
+	path := b.Host + "/internal/" + BillingServiceBasePath + "/subscription-info/" + accountID.String()
+	request, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, e3dbClients.NewError(err.Error(), path, 0)
+	}
+	err = e3dbClients.MakeE3DBServiceCall(b.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+func (b *E3dbBillingClient) AccountSubscriptionStatus(ctx context.Context) (*GetAccountStatusResponse, error) {
+	var result *GetAccountStatusResponse
+	path := b.Host + BillingServiceBasePath + "/subscription/status"
+	request, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, e3dbClients.NewError(err.Error(), path, 0)
+	}
+	err = e3dbClients.MakeE3DBServiceCall(b.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+func (b *E3dbBillingClient) Unsubscribe(ctx context.Context) (*GetAccountStatusResponse, error) {
+	var result *GetAccountStatusResponse
+	path := b.Host + BillingServiceBasePath + "/unsubscribe"
+	request, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, e3dbClients.NewError(err.Error(), path, 0)
+	}
+	err = e3dbClients.MakeE3DBServiceCall(b.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
+func (b *E3dbBillingClient) Resubscribe(ctx context.Context) (*GetAccountStatusResponse, error) {
+	var result *GetAccountStatusResponse
+	path := b.Host + BillingServiceBasePath + "/resubscribe"
+	request, err := e3dbClients.CreateRequest("GET", path, nil)
 	if err != nil {
 		return result, e3dbClients.NewError(err.Error(), path, 0)
 	}
