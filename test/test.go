ffd91bf8ddd1b15a1cfe14558d61556369f672a3
michael <willmichael@tozny.com>
2019-06-26 12:04:03 -0700

    initial testing refactor

--- test/test.go
+++ test/test.go
@@ -9,6 +9,7 @@ import (
 
 	e3dbClients "github.com/tozny/e3db-clients-go"
 	"github.com/tozny/e3db-clients-go/accountClient"
+	"github.com/tozny/e3db-clients-go/clientServiceClient"
 	"github.com/tozny/e3db-go/v2"
 )
 
@@ -79,4 +80,61 @@ func MakeE3DBAccount(t *testing.T, accounter *accountClient.E3dbAccountClient, a
 	accountClientConfig.APIKey = accountResponse.Account.Client.APIKeyID
 	accountClientConfig.APISecret = accountResponse.Account.Client.APISecretKey
 	return accountClientConfig, accountResponse, err
+}
+
+// RegisterClient is a helper method to generate a client with client service,
+// returns a registration response and an empty config for said client.
+// On error t.Fatal is called halting test execution.
+func RegisterClient(t *testing.T, clientServiceHost string, registrationToken string, clientName string) (*clientServiceClient.ClientRegisterResponse, e3dbClients.ClientConfig) {
+	// init empty config to make registration requests
+	var registrationResponse *clientServiceClient.ClientRegisterResponse
+	userClientConfig := e3dbClients.ClientConfig{
+		Host: clientServiceHost,
+	}
+	publicKey, _, err := e3db.GenerateKeyPair()
+	if err != nil {
+		t.Fatalf("Failed generating public key pair %s", err)
+	}
+	signingKey, _, err := e3db.GenerateKeyPair()
+	if err != nil {
+		t.Fatalf("Failed generating signing key pair %s", err)
+	}
+	unAuthedClientServiceClient := clientServiceClient.New(userClientConfig)
+	// Register user
+	userRegister := clientServiceClient.ClientRegisterRequest{
+		RegistrationToken: registrationToken,
+		Client: clientServiceClient.ClientRegisterInfo{
+			Name:        clientName,
+			Type:        "general",
+			PublicKeys:  map[string]string{"curve25519": publicKey},
+			SigningKeys: map[string]string{"ed25519": signingKey},
+		},
+	}
+	ctx := context.Background()
+	registrationResponse, err = unAuthedClientServiceClient.Register(ctx, userRegister)
+	if err != nil {
+		t.Fatalf("unable to register user %+v, err: %s\n", userRegister, err)
+	}
+	userClientConfig.Host = ""                     // clear host, make user define
+	userClientConfig.AuthNHost = clientServiceHost // client service is now auth-service
+	userClientConfig.APIKey = registrationResponse.APIKeyID
+	userClientConfig.APISecret = registrationResponse.APISecret
+	return registrationResponse, userClientConfig
+}
+
+func CreateRegistrationToken(t *testing.T, queenAccountClient *accountClient.E3dbAccountClient, accountServiceJWT string, clientServiceHost string) string {
+	createRegParams := accountClient.CreateRegistrationTokenRequest{
+		AccountServiceToken: accountServiceJWT,
+		TokenPermissions: accountClient.TokenPermissions{
+			Enabled: true,
+			OneTime: false,
+		},
+	}
+	// create registration token
+	ctx := context.Background()
+	createdTokenResp, err := queenAccountClient.CreateRegistrationToken(ctx, createRegParams)
+	if err != nil {
+		t.Fatalf("could not create registration token %s\n", err)
+	}
+	return createdTokenResp.Token
 }
