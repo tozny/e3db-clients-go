ed7b1307269fd5e11b49957ba86eb206640a4db7
michael <willmichael@tozny.com>
2019-07-22 16:18:45 -0700

    storage service initial client

---
+++ storageClient/storageClient.go
@@ -0,0 +1,55 @@
+package storageClient
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/tozny/e3db-clients-go"
+)
+
+const (
+	storageServiceBasePath = "/v2/storage"
+)
+
+//StorageClient implements an http client for communication with the metrics service.
+type StorageClient struct {
+	ClientID    string
+	SigningKeys e3dbClients.SigningKeys
+	Host        string // host will generally need to be cyclops service to get the X-Tozny-Auth header
+	httpClient  *http.Client
+}
+
+func (c *StorageClient) WriteNote(ctx context.Context, params Note) (*Note, error) {
+	var result *Note
+	path := c.Host + storageServiceBasePath + "/notes"
+	request, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeSignedServiceCall(ctx, request, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
+
+func (c *StorageClient) ReadNote(ctx context.Context, noteID string) (*Note, error) {
+	var result *Note
+	path := c.Host + storageServiceBasePath + "/notes"
+	request, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, err
+	}
+	urlParams := request.URL.Query()
+	urlParams.Set("note_id", noteID)
+	request.URL.RawQuery = urlParams.Encode()
+	err = e3dbClients.MakeSignedServiceCall(ctx, request, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
+
+// New returns a new E3dbSearchIndexerClient for authenticated communication with a Search Indexer service at the specified endpoint.
+func New(config e3dbClients.ClientConfig) StorageClient {
+	return StorageClient{
+		Host:        config.Host,
+		SigningKeys: config.SigningKeys,
+		ClientID:    config.ClientID,
+		httpClient:  &http.Client{},
+	}
+}
