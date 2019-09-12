5fc87577daea908eb4d5f2e8a18cb7a083056b44
Eli Fabens <efabens@tozny.com>
2019-09-12 10:26:13 -0700

    Add Target to create notification request

--- notificationClient/api.go
+++ notificationClient/api.go
@@ -10,6 +10,12 @@ const (
 	ChannelEmail = "email"
 )
 
+// UserContactInfo is specfic information about the entity to be contacted
+type UserContactInfo struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
 // CreateNotificationRequest is the api-level struct which represents an object expected by notification-service's POST / endpoint
 type CreateNotificationRequest struct {
 	Template  string            `json:"template"`
@@ -18,7 +24,8 @@ type CreateNotificationRequest struct {
 	AccountID uuid.UUID         `json:"account_id"`
 	Nonce     string            `json:"nonce"`
 	// SendAt optionally specifies a suggested time when the notification should be sent
-	SendAt time.Time `json:"send_at,omitempty"`
+	SendAt time.Time       `json:"send_at,omitempty"`
+	Target UserContactInfo `json:"target,omitempty"`
 }
 
 // NotificationMeta is the metadata about a notification which, eg, returned when creating a notification
