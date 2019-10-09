fc75e060a807168eeff38f82e9b432666f57b857
michael <willmichael@tozny.com>
2019-10-09 12:35:08 -0700

    active user aggregation call

--- metricClient/metricClient.go
+++ metricClient/metricClient.go
@@ -42,6 +42,18 @@ func (c *MetricClient) RequestsMetrics(ctx context.Context, params APIMetricsReq
 	return result, err
 }
 
+// ActiveUsers gets the active users for an account from the metrics service.
+func (c *MetricClient) ActiveUsers(ctx context.Context, params ActiveUserAggregateRequest) (*ActiveUserAggregateResponse, error) {
+	var result *ActiveUserAggregateResponse
+	path := c.Host + "/" + MetricServiceBasePath + "requests/aggregations/active-users"
+	request, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeE3DBServiceCall(c.E3dbAuthClient, ctx, request, &result)
+	return result, err
+}
+
 // New returns a new E3dbSearchIndexerClient for authenticated communication with a Search Indexer service at the specified endpoint.
 func New(config e3dbClients.ClientConfig) MetricClient {
 	authService := authClient.New(config)
