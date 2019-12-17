194ca12c72efb86d95ec8656f19d7f119581f43e
michael <willmichael@tozny.com>
2019-12-17 15:58:32 -0800

    direct mobile push api

--- notificationClient/notificationClient.go
+++ notificationClient/notificationClient.go
@@ -20,10 +20,22 @@ const (
 	NotificationServiceBasePath = "/v1/notification"
 )
 
-// CreateNotification pushes a notification via an internal endpoint. Requires the bootstrap client.
+// CreateNotification pushes a notification, requires the bootstrap client.
 func (nc *ToznyNotificationClient) CreateNotification(ctx context.Context, params CreateNotificationRequest) (*NotificationMeta, error) {
 	var result *NotificationMeta
 	path := nc.Host + NotificationServiceBasePath + "/"
+	request, err := e3dbClients.CreateRequest(http.MethodPost, path, params)
+	if err != nil {
+		return result, e3dbClients.NewError(err.Error(), path, 0)
+	}
+	err = e3dbClients.MakeSignedServiceCall(ctx, request, nc.SigningKeys, nc.ClientID, &result)
+	return result, err
+}
+
+// DirectMobilePush sends a mobile push notification to a single user, requires the bootstrap client.
+func (nc *ToznyNotificationClient) DirectMobilePush(ctx context.Context, params DirectMobilePushRequestWithPayload) (*PushResponse, error) {
+	var result *PushResponse
+	path := nc.Host + NotificationServiceBasePath + "/direct-mobile-push"
 	request, err := e3dbClients.CreateRequest(http.MethodPost, path, params)
 	if err != nil {
 		return result, e3dbClients.NewError(err.Error(), path, 0)
