7660a8fc819ec57d38afa7850c3fc40cd72b25e4
Michael Lee <willmichael@tozny.com>
2020-03-24 10:42:13 -0700

    Add changes to identity spec and crypto method to aid in testing (#98)
    * add totp fields
    
    * generate encryption keys
    
    * add encryption key
    
    * adjust naming for totp verification

--- identityClient/api.go
+++ identityClient/api.go
@@ -177,14 +177,16 @@ type InitiateUserChallengeResponse struct {
 }
 
 type InitiateRegisterDeviceRequest struct {
-	TempPublicKey string `json:"temporary_public_key"`
+	TempPublicKey           string `json:"temporary_public_key"`
+	TempPublicEncryptionKey string `json:"temporary_public_encryption_key"`
 }
 
 // InitiateRegisterDeviceResponse
 type InitiateRegisterDeviceResponse struct {
-	RegistrationID string `json:"registration_id"`
-	Username       string `json:"username"`
-	Realm          string `json:"realm"`
+	RegistrationID      string `json:"registration_id"`
+	Username            string `json:"username"`
+	Realm               string `json:"realm"`
+	EncryptedTOTPSecret string `json:"encrypted_totp_secret"`
 }
 
 type CompleteUserDeviceRegisterRequest struct {
@@ -196,6 +198,7 @@ type CompleteUserDeviceRegisterRequest struct {
 	DeviceID        string `json:"device_id"`         // Unique device identifier
 	DeviceName      string `json:"device_name"`       // Human readable device name
 	DevicePublicKey string `json:"device_public_key"` // PublicKey to permanently stored PrivateKey on device
+	TOTP            string `json:"totp"`
 }
 
 type CompleteChallengeRequest struct {
