ffbbe48d9f58b5b0e849564917e08c6ac494a84d
michael <willmichael@tozny.com>
2020-03-31 12:10:40 -0700

    add back in missing api struct

--- clientServiceClient/api.go
+++ clientServiceClient/api.go
@@ -123,3 +123,8 @@ type IssueEmailChallengeRequest struct {
 type IssueEmailChallengeResponse struct {
 	Link string `json:"link"`
 }
+
+type VerifyEmailChallengeRequest struct {
+	ID  string `json:"id"`
+	OTP string `json:"otp"`
+}
