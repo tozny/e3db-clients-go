6e97ddb8b23c1d14eb4c3c080d1f3fc05f96d83d
michael <willmichael@tozny.com>
2020-03-31 11:53:24 -0700

    remove unused structs

--- clientServiceClient/api.go
+++ clientServiceClient/api.go
@@ -123,12 +123,3 @@ type IssueEmailChallengeRequest struct {
 type IssueEmailChallengeResponse struct {
 	Link string `json:"link"`
 }
-
-type VerifyEmailChallengeRequest struct {
-	ID  string `json:"id"`
-	OTP string `json:"otp"`
-}
-
-type VerifyEmailChallengeResponse struct {
-	Data string `json:"data"`
-}
