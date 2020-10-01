a9126738065fcd5be75980ea4f49af3929a95ec2
veen <benjamin@survantjames.com>
2020-10-01 14:59:40 -0700

    add application access type

--- identityClient/api.go
+++ identityClient/api.go
@@ -299,6 +299,7 @@ type Application struct {
 // ApplicationOIDCSettings wraps settings for an OpenID Connect enabled application
 type ApplicationOIDCSettings struct {
 	// (Optional) The URL to append to any relative URLs
+	AccessType string `json:"access_type"`
 	RootURL string `json:"root_url"`
 	StandardFlowEnabled bool `json:"standard_flow_enabled"`
 	BaseURL string `json:"base_url"`
