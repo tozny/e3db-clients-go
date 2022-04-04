751c47eb972df4edadfddae8fd43ddf1797de15c
Lily Tsou <lily@tozny.com>
2022-04-04 13:03:11 -0700

    [PLAT-942] Adds a new test for GroupsPost, changes Capability name to Capabilities

--- storageClient/api.go
+++ storageClient/api.go
@@ -41,7 +41,7 @@ type CreateGroupRequest struct {
 	PublicKey         string   `json:"public_key"`
 	Description       string   `json:"description"`
 	EncryptedGroupKey string   `json:"encrypted_group_key"`
-	Capability        []string `json:"capability"`
+	Capabilities      []string `json:"capability"` //The capability or capabilities (if any) to be added to the client creating a group.
 }
 
 // Group wraps values related to managing client membership and capabilities for resources such as records.
