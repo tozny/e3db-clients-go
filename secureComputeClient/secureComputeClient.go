a9a85bb01135d16ff874987acdb297f9facc9938
Katie Rosas <katie@tozny.com>
2022-05-04 16:59:28 -0700

    [PLAT-976] Client side endpoints

--- secureComputeClient/secureComputeClient.go
+++ secureComputeClient/secureComputeClient.go
@@ -63,3 +63,78 @@ func (c *E3dbSecureComputeClient) HealthCheck(ctx context.Context) error {
 	err = e3dbClients.MakeRawServiceCall(c.requester, req, nil)
 	return err
 }
+
+// Subscribe allows a TozStore Client to subscribe for an available computation for their data
+func (c *E3dbSecureComputeClient) Subscribe(ctx context.Context, params SubscriptionRequest) (*SubscriptionResponse, error) {
+	var result *SubscriptionResponse
+	path := c.Host + secureComputeServiceBasePath + "/subscription"
+	req, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
+
+// UpdateSubscription Updates information on subscribed computation
+func (c *E3dbSecureComputeClient) UpdateSubscription(ctx context.Context, params UpdateSubscriptionRequest) error {
+	path := c.Host + secureComputeServiceBasePath + "/subscription"
+	req, err := e3dbClients.CreateRequest("PUT", path, params)
+	if err != nil {
+		return err
+	}
+	return e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, nil)
+}
+
+// FetchSubsciption Fetches all Computations the TozStore Client is registered for
+func (c *E3dbSecureComputeClient) FetchSubsciptions(ctx context.Context, params FetchSubscriptionsRequest) (*ComputationResponse, error) {
+	var result *ComputationResponse
+	path := c.Host + secureComputeServiceBasePath + "/subscription"
+	req, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, err
+	}
+	urlParams := req.URL.Query()
+	urlParams.Set("client_id", params.ToznyClientID.String())
+	req.URL.RawQuery = urlParams.Encode()
+	err = e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
+
+// Unsubscribe ends the subscription for a TozStore Client from a computation
+func (c *E3dbSecureComputeClient) Unsubscribe(ctx context.Context, params UnsubscribeRequest) error {
+	path := c.Host + secureComputeServiceBasePath + "/subscription"
+	req, err := e3dbClients.CreateRequest("DELETE", path, nil)
+	if err != nil {
+		return err
+	}
+	urlParams := req.URL.Query()
+	urlParams.Set("client_id", params.ToznyClientID.String())
+	urlParams.Set("computation_id", params.ComputationID.String())
+	req.URL.RawQuery = urlParams.Encode()
+	return e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, nil)
+}
+
+// ComputeAnalysis runs a computation for a subscribed Tozstore Client
+func (c *E3dbSecureComputeClient) ComputeAnalysis(ctx context.Context, params ComputationRequest) (*ComputationResponse, error) {
+	var result *ComputationResponse
+	path := c.Host + secureComputeServiceBasePath + "/compute/" + params.ComputationID.String()
+	req, err := e3dbClients.CreateRequest("POST", path, params)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
+
+// FetchAvailableComputations fetches all available computations to the TozStore Client
+func (c *E3dbSecureComputeClient) FetchAvailableComputations(ctx context.Context) (*ComputationResponse, error) {
+	var result *ComputationResponse
+	path := c.Host + secureComputeServiceBasePath + "/compute"
+	req, err := e3dbClients.CreateRequest("GET", path, nil)
+	if err != nil {
+		return result, err
+	}
+	err = e3dbClients.MakeSignedServiceCall(ctx, c.requester, req, c.SigningKeys, c.ClientID, &result)
+	return result, err
+}
